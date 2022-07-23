569319d86cb9d64c6dde74e265aa0a94e644b08e
samyarsworld <samyarfarjam@outlook.com>
2022-07-22 22:25:22 -0400

    middleware and persistent form

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
+import { Loading } from './LoadingComponent';
+
+function RenderCard({ item, errMess, isLoading }) {
+    if (isLoading) {
+        return (
+            <Loading />
+        );
+    }
+    else if (errMess) {
+        return (
+            <h4>{errMess}</h4>
+        );
+    }
+
+    else {
+        return (
+            <React.Fragment>
+                <Card>
+                    <CardImg src={item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </React.Fragment>
+
+        );
+    }
+
+}
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish}
+                        isLoading={props.dishesLoading}
+                        errMess={props.dishesErrMess} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import DishDetail from './DishDetailComponent';
+import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
+
+import { actions } from 'react-redux-form';
+
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
+
+});
+
+
+
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
+
+    render() {
+
+        const HomePage = () => {
+
+            return (
+                <Home
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
+                />
+            );
+        };
+
+        return (
+
+            <React.Fragment>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+
+
